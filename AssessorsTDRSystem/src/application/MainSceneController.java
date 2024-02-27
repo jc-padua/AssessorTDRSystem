f349acf0726e17f1975e36d740e8cc475f3698b6
Jork Cedric <jcp093001@gmail.com>
2024-02-27 11:10:42 +0800

    add fx:id on each components

---
+++ AssessorsTDRSystem/src/application/MainSceneController.java
@@ -0,0 +1,23 @@
+package application;
+
+import javafx.fxml.FXML;
+
+import javafx.scene.control.Button;
+
+import javafx.scene.control.TextField;
+
+public class MainSceneController {
+	@FXML
+	private TextField tfPIN;
+	@FXML
+	private TextField tfSNUMBER;
+	@FXML
+	private TextField tfOWNER;
+	@FXML
+	private TextField tfLOCATION;
+	@FXML
+	private Button btnUPLOAD;
+	@FXML
+	private Button btnADD;
+
+}
