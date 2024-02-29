abe5a052b658961d96609aca5f3093c566f21785
Jork Cedric <jcp093001@gmail.com>
2024-02-29 09:13:26 +0800

    Merge branch 'master' of https://github.com/jc-padua/AssessorTDRSystem.git

--- AssessorsTDRSystem/src/application/MainSceneController.java
+++ AssessorsTDRSystem/src/application/MainSceneController.java
@@ -1,12 +1,20 @@
 package application;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+
 import javafx.fxml.FXML;
-
+import javafx.event.ActionEvent;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
-
 import javafx.scene.control.TextField;
 
 public class MainSceneController {
+	
+	
+	Connection con;
+	PreparedStatement pst;
+	
 	@FXML
 	private TextField tfPIN;
 	@FXML
@@ -20,4 +28,31 @@ public class MainSceneController {
 	@FXML
 	private Button btnADD;
 
+	 @FXML
+	    void addData(ActionEvent event) {
+		 
+		 String pin = tfPIN.getText();
+		 String snumber = tfSNUMBER.getText();
+		 String owner = tfOWNER.getText();
+		 String location = tfLOCATION.getText();
+		 
+		 Alert alert = new Alert(Alert.AlertType.ERROR);
+		 try {
+			 if (pin.isEmpty() && snumber.isEmpty() && owner.isEmpty() && location.isEmpty()) {
+				 alert.setTitle("Input Validation");
+				 alert.setHeaderText("Please fill the input field");
+				 alert.show();
+				 return;
+			 } else {
+				 System.out.println(pin);
+				 System.out.println(snumber);
+				 System.out.println(owner);
+				 System.out.println(location);
+			 }
+			
+		} catch (Exception e) {
+			System.out.println(e);
+		}
+	    }
+	
 }
