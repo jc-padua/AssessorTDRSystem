3b30c2ebf7ce427da7b26d24c6a2616d1485e68c
Lhex Aaron Ferreras <feiklr05@gmail.com>
2024-03-04 16:14:01 +0800

    Add to DB

--- AssessorsTDRSystem/src/application/MainSceneController.java
+++ AssessorsTDRSystem/src/application/MainSceneController.java
@@ -1,7 +1,12 @@
 package application;
 
 import java.sql.Connection;
+import java.sql.DriverManager;
 import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+import javax.swing.JOptionPane;
+
 import javafx.scene.control.DatePicker;
 
 import javafx.fxml.FXML;
@@ -40,24 +45,39 @@ public class MainSceneController {
 		 String location = tfLOCATION.getText();
 		 
 		 try {
-			 if (pin.isEmpty() && snumber.isEmpty() && owner.isEmpty() && location.isEmpty()) {
-				 Alert alert = new Alert(Alert.AlertType.ERROR);
-				 alert.setTitle("Input Validation");
-				 alert.setHeaderText("Please fill the input field");
-				 alert.show();
-				 return;
-			 } else {
-				 Alert alert = new Alert(Alert.AlertType.INFORMATION);
-				 alert.setTitle("Sample");
-				 alert.setHeaderText("Sample");
-				 alert.setContentText(pin + "\n" + snumber + "\n" + owner + "\n" + location);
-				 alert.show();
-				 return;
+//			 if (pin.isEmpty() && snumber.isEmpty() && owner.isEmpty() && location.isEmpty()) {
+//				 Alert alert = new Alert(Alert.AlertType.ERROR);
+//				 alert.setTitle("Input Validation");
+//				 alert.setHeaderText("Please fill the input field");
+//				 alert.show();
+//				 return;
+//			 } else {
+//				 Alert alert = new Alert(Alert.AlertType.INFORMATION);
+//				 alert.setTitle("Sample");
+//				 alert.setHeaderText("Sample");
+//				 alert.setContentText(pin + "\n" + snumber + "\n" + owner + "\n" + location);
+//				 alert.show();
+//				 return;
+//			 }
+			 
+			 Class.forName("com.mysql.cj.jdbc.Driver");
+			 con = DriverManager.getConnection("jdbc:mysql://localhost/assessor_db", "root", "admin");
+			 pst = con.prepareStatement("INSERT INTO tax_dec (pin,serial_number,owner,location) VALUES (?,?,?,?) ");
+			 pst.setString(1, pin);
+			 pst.setString(2, snumber);
+			 pst.setString(3, owner);
+			 pst.setString(4, location);
+			 
+			 int affectedRows = pst.executeUpdate();
+			 if (affectedRows > 0) {
+				 JOptionPane.showMessageDialog(null, "Successfully Added!");
 			 }
 			
-		} catch (Exception e) {
-			System.out.println(e);
-		}
-	    }
+		 } catch (SQLException e) {
+			e.printStackTrace();
+		 } catch (ClassNotFoundException e) { 
+			 e.printStackTrace();
+		 }
+	}
 	
 }
