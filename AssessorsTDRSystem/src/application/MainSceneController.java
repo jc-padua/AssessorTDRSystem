fab5e4c0d54ad10b580e0d981df64fbe3de48455
Lhex Aaron Ferreras <feiklr05@gmail.com>
2024-03-05 14:12:53 +0800

    TableView - WORKING

--- AssessorsTDRSystem/src/application/MainSceneController.java
+++ AssessorsTDRSystem/src/application/MainSceneController.java
@@ -4,18 +4,29 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import java.sql.ResultSet;
+import java.sql.Statement;
 
 import javax.swing.JOptionPane;
 
 import javafx.scene.control.DatePicker;
 
+import java.net.URL;
+import java.util.ResourceBundle;
+
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.event.ActionEvent;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
 
-public class MainSceneController {
+public class MainSceneController implements Initializable {
 	
 	
 	Connection con;
@@ -35,10 +46,79 @@ public class MainSceneController {
 	private Button btnADD;
 	@FXML
 	private DatePicker dpDATE;
+	@FXML
+	private TableView<td_data> TDTaxDec;
+	@FXML
+	private TableColumn<td_data, String> pinColumn;
+	@FXML
+    private TableColumn<td_data, String> seriesColumn;
+    @FXML
+    private TableColumn<td_data, String> ownerColumn;
+    @FXML
+    private TableColumn<td_data, String> locationColumn;
 
+    private ObservableList<td_data> taxDecList;
+    
+    public MainSceneController() {
+    	taxDecList = FXCollections.observableArrayList();
+    }
+   
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+    	pinColumn.setCellValueFactory(cellData -> new SimpleStringProperty(cellData.getValue().getPin()));
+        seriesColumn.setCellValueFactory(cellData -> new SimpleStringProperty(cellData.getValue().getSnumber()));
+        ownerColumn.setCellValueFactory(cellData -> new SimpleStringProperty(cellData.getValue().getOwner()));
+        locationColumn.setCellValueFactory(cellData -> new SimpleStringProperty(cellData.getValue().getLocation()));
+        
+        TDTaxDec.setItems(taxDecList);
+       
+        loadDataFromDatabase();
+    }
+
+    private void loadDataFromDatabase() {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            con = DriverManager.getConnection("jdbc:mysql://localhost/assessor_db", "root", "admin");
+            Statement stmt = con.createStatement();
+            ResultSet rs = stmt.executeQuery("SELECT * FROM tax_dec");
+
+            while (rs.next()) {
+                String pin = rs.getString("pin");
+                String series = rs.getString("series_number");
+                String owner = rs.getString("owner");
+                String location = rs.getString("location");
+
+                td_data taxData = new td_data(pin, series, owner, location);
+                taxDecList.add(taxData);
+            }
+
+            rs.close();
+            stmt.close();
+        } catch (SQLException | ClassNotFoundException e) {
+            e.printStackTrace();
+            showErrorAlert("Error loading data from the database");
+        } finally {
+            // Close resources
+            try {
+                if (con != null) {
+                    con.close();
+                }
+            } catch (SQLException ex) {
+                ex.printStackTrace();
+            }
+        }
+    }
+
+    private void showErrorAlert(String message) {
+        Alert alert = new Alert(Alert.AlertType.ERROR);
+        alert.setTitle("Error");
+        alert.setHeaderText(null);
+        alert.setContentText(message);
+        alert.show();
+    }
+    
 	 @FXML
-	    void addData(ActionEvent event) {
-		 
+	 void addData(ActionEvent event) {
 		 String pin = tfPIN.getText();
 		 String snumber = tfSNUMBER.getText();
 		 String owner = tfOWNER.getText();
@@ -70,14 +150,48 @@ public class MainSceneController {
 			 
 			 int affectedRows = pst.executeUpdate();
 			 if (affectedRows > 0) {
-				 JOptionPane.showMessageDialog(null, "Successfully Added!");
+				 Alert alert = new Alert(Alert.AlertType.INFORMATION);
+	             alert.setTitle("Success");
+	             alert.setHeaderText(null);
+	             alert.setContentText("Successfully Added!");
+	             alert.show();
+	             
+	             td_data newTaxData = new td_data(pin, snumber, owner, location);
+	             taxDecList.add(newTaxData);
+	             TDTaxDec.setItems(taxDecList);
+	             
+	             clearInputFields();
 			 }
+			 
 			
 		 } catch (SQLException e) {
 			e.printStackTrace();
+			Alert alert = new Alert(Alert.AlertType.ERROR);
+	        alert.setTitle("Error");
+	        alert.setHeaderText(null);
+	        alert.setContentText("Error occurred while adding data!");
+	        alert.show();
 		 } catch (ClassNotFoundException e) { 
 			 e.printStackTrace();
+		 } finally {
+			 try {
+				 if (pst != null) {
+					 pst.close();
+				 }
+				 if (con != null) {
+					 con.close();
+				 }
+			 } catch (SQLException ex) {
+				 ex.printStackTrace();
+			 }
 		 }
+	}
+	private void clearInputFields() {
+		// TODO Auto-generated method stub
+		tfPIN.clear();
+        tfSNUMBER.clear();
+        tfOWNER.clear();
+        tfLOCATION.clear();
 	}
 	
 }
